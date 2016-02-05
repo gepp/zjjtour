1aa4af7b40512145bd607636607f48d5a3afc522
jdk2010 <jdk2010@163.com>
2016-02-06 00:02:54 +0800

    1232

---
+++ src/main/java/com/jdk2010/util/ZjjMsgUtil.java
@@ -0,0 +1,67 @@
+package com.jdk2010.util;
+
+import com.jdk2010.framework.util.HttpUtil;
+import com.jdk2010.framework.util.MD5Utils;
+
+public class ZjjMsgUtil {
+	public static final String URL="http://ststest.travelzjj.com:15404/sts-app/Api/member/";
+	public static final String KEY="YD#$kweri8889";
+	public static final String USERNAME="v7line001";
+	
+	public static String getSign(String param){
+		return MD5Utils.md5(USERNAME+param+KEY);
+	}
+	
+	//发送验证码
+	public static String sendCode(String mobile,String type){
+ 		String methodName="sendCode";
+		String paramsJson = "{\"ctel\":\""+mobile+"\",\"type\":\""+type+"\"}";
+		String sign = MD5Utils.md5(USERNAME+paramsJson+KEY);
+		String posturl=URL+ methodName+"?format=json&user="+USERNAME+"&paramsJson="+ paramsJson + "&sign="+sign;
+		System.out.println("sendCode-url:"+posturl);
+		String returnStr=HttpUtil.post(posturl,"");
+		System.out.println("returnstr:"+returnStr);
+		return returnStr;
+	}
+	
+	//是否注册
+	public static String isExists(String mobile){
+		String methodName="isExists";
+		String paramsJson = "{\"cloginname\":\""+mobile+"\"}";
+		String sign = MD5Utils.md5(USERNAME+paramsJson+KEY);
+		String posturl=URL+ methodName+"?format=json&user="+USERNAME+"&paramsJson="+ paramsJson + "&sign="+sign;
+		System.out.println("isExists-url:"+posturl);
+		String returnStr=HttpUtil.post(posturl,"");
+		System.out.println("returnstr:"+returnStr);
+		return returnStr;
+	}
+	
+	//注册
+	public static String registerMember(String mobile,String verifyCode,String cpassword){
+		String methodName="registerMember";
+		String paramsJson ="{\"verifyCode\":\""+verifyCode+"\",data:{\"ctel\":\""+mobile+"\",\"cpassword\":\""+cpassword+"\",\"cnickname\":\""+mobile+"\"}}";
+		String sign = MD5Utils.md5(USERNAME+paramsJson+KEY);
+		String posturl=URL+ methodName+"?format=json&user="+USERNAME+"&paramsJson="+ paramsJson + "&sign="+sign;
+		System.out.println("registerMember-url:"+posturl);
+		String returnStr=HttpUtil.post(posturl,"");
+		System.out.println("returnstr:"+returnStr);
+		return returnStr;
+	}
+	
+	//登录
+	public static String login(String mobile,String cpassword){
+		String methodName="memberLogin";
+		String paramsJson ="{\"cloginname\":\""+mobile+"\",\"cpassword\":\""+cpassword+"\"}";
+		String sign = MD5Utils.md5(USERNAME+paramsJson+KEY);
+		String posturl=URL+ methodName+"?format=json&user="+USERNAME+"&paramsJson="+ paramsJson + "&sign="+sign;
+		System.out.println("memberLogin-url:"+posturl);
+		String returnStr=HttpUtil.post(posturl,"");
+		System.out.println("returnstr:"+returnStr);
+		return returnStr;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ZjjMsgUtil.login("18621790300","e10adc3949ba59abbe56e057f20f883e"));
+	}
+	
+}
